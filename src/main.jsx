4364c494596d813652c70386adc595de181b5bf8
Sanjar <truesanjar@gmail.com>
2026-09-10 21:14:49 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "./registration/firebase"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <App />
